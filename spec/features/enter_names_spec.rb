4c858fdd62b197dd08395c00a346facd515c9e64
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-10 20:38:56 +0100

    Using a helper for the sign in and play process

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,10 +3,7 @@ require_relative '../../app'
 
 feature "fill in a form and see your name" do 
   scenario "player enters their name and sees it on screen" do 
-    visit ('/')
-    fill_in :player_1_name, with: "Adrienne"
-    fill_in :player_2_name, with: "Bev"
-    click_button "Submit"
+    sign_in_and_play
     
     save_and_open_page
    
--- spec/features/see_opponent_points.rb
+++ spec/features/see_opponent_points.rb
@@ -3,10 +3,7 @@ require_relative '../../app'
 
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Adrienne'
-    fill_in :player_2_name, with: 'Bev'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Adrienne: 60HP'
   end
 end
